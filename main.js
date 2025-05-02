dd51227e248ad429ea0be4b6dd05b52c29e1c16b
Yewan Na <135602531+nyw0102@users.noreply.github.com>
2025-05-02 21:46:11 +0900

    Update main.js

--- main.js
+++ main.js
@@ -6,15 +6,18 @@ import { resetState, updateAllComponents } from './state.js';
 
 let rawData = [];
 
+// 날짜 파싱 함수 (e.g., "04-Jan-22")
 const parseDate = d3.timeParse("%d-%b-%y");
 
 // 데이터 불러오기 및 초기화
 (async function () {
-  rawData = await d3.csv("Chocolate-Sales.csv", d => ({
+  rawData = await d3.csv("chocolate_sales_cleaned.csv", d => ({
     date: parseDate(d.Date),                             // "04-Jan-22" 형식 파싱
     sales: +d.Amount.replace(/[$,]/g, ""),              // "$5,320" → 5320 숫자로 변환
     category: d.Country,
     product: d.Product,
+    salesperson: d["Sales Person"],
+    boxes: +d["Boxes Shipped"],
     id: d["Sales Person"] + "_" + d.Date
   }));
 
